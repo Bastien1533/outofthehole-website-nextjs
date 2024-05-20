00d9227aae665184d34d682aa6f2dac6731710a9
Bastien Pinaud <bastien.pinaud@epita.fr>
2024-05-21 01:20:15 +0200

    refactor(Website): :recycle:  SSR for Team page

--- src/components/github_card.tsx
+++ src/components/github_card.tsx
@@ -43,36 +43,14 @@ export function GithubRepoCard(org_name:string,  repo_name:string){
 }
 
 
-export function GithubCard(name:string, gh_name: string) {
-    const [data, setData] = useState(null)
-    const [isLoading, setLoading] = useState(true)
-    const token: string = process.env.NEXT_PUBLIC_SECRET_KEY == undefined ? "" : process.env.NEXT_PUBLIC_SECRET_KEY;
+export function GithubCard(name:string, gh_name: string, team_data:string) {
+    let profile = JSON.parse(team_data)[gh_name] as GitHubProfile
+    return (<a href={profile.html_url}>
+        <div className={"flex items-center py-2 px-5 gap-7 bg-oth_yellow rounded-lg"}>
+            <Image className={"rounded-full"} src={profile.avatar_url} alt={"test"} width={100} height={100}></Image>
+            <span>{name}</span>
+        </div>
+    </a>)
     
-    
-    useEffect(() => {
-        fetch(`https://api.github.com/users/${gh_name}`, {
-            method: "GET",
-            headers:  {
-                "Authorization": `Bearer ${token}`,
-            }
-        })
-            .then((res) => res.json())
-            .then((data) => {
-                setData(data)
-                setLoading(false)
-            })
-    }, [])
-
-    if (isLoading) return <p>Loading...</p>
-    if (!data) return <p>No profile data</p>
-    else {
-        let profile = data as GitHubProfile
-        return (<a href={profile.html_url}>
-            <div className={"flex items-center py-2 px-5 gap-7 bg-oth_yellow rounded-lg"}>
-                <Image className={"rounded-full"} src={profile.avatar_url} alt={"test"} width={100} height={100}></Image>
-                <span>{name}</span>
-            </div>
-        </a>)
-    }
     
 }
--- src/pages/team.tsx
+++ src/pages/team.tsx
@@ -1,18 +1,41 @@
 import PageLayout from "@/pages/layout";
 import {GithubCard} from "@/components/github_card";
+import {GitHubProfile, RepoRelease} from "@/models/github";
+import type {GetServerSideProps, InferGetServerSidePropsType} from "next";
 
 
-export default function TeamPage() {
+export const getServerSideProps = (async () => {
+
+    const token: string = process.env.NEXT_PUBLIC_SECRET_KEY == undefined ? "" : process.env.NEXT_PUBLIC_SECRET_KEY;
+    
+    // Team Data Fetching
+    const team = (process.env.NEXT_PUBLIC_TEAM== undefined ? "" : process.env.NEXT_PUBLIC_TEAM).split(",");
+    let team_data_dict : {} = {};
+    for (const member in team) {
+        const res = await fetch(`https://api.github.com/users/${team[member]}`, {
+            method: "GET",
+            headers: {
+                "Authorization" : `Bearer ${token}`
+            }});
+        (team_data_dict as any)[team[member]] = await res.json()
+    }
+    let team_data = JSON.stringify(team_data_dict)
+    
+    
+    return { props: { team_data } }
+}) satisfies GetServerSideProps<{ team_data: string }>
+
+export default function TeamPage({team_data,} : InferGetServerSidePropsType<typeof getServerSideProps>) {
     return <PageLayout>
         <div className={"flex justify-evenly"}>
             <div className={"flex flex-col gap-7"}>
                 <span className={"mt-24 text-oth_yellow text-7xl" }>The Team</span>
                 <div className={"flex justify-center"}>
                     <div className={"grid grid-cols-2 w-fit gap-7"}>
-                        {GithubCard("Bastien Pinaud","Bastien1533")}
-                        {GithubCard("Louis Gallet","l0u1sg")}
-                        {GithubCard("Titouan Lamy","Drakwrizz")}
-                        {GithubCard("Hugo Cohen","Yoplyy")}
+                        {GithubCard("Bastien Pinaud","Bastien1533", team_data)}
+                        {GithubCard("Louis Gallet","l0u1sg", team_data)}
+                        {GithubCard("Titouan Lamy","Drakwrizz", team_data)}
+                        {GithubCard("Hugo Cohen","Yoplyy", team_data)}
                     </div>
                 </div>
             </div>
